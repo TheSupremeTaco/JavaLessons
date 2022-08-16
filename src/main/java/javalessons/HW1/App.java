6d8573b86684626570bbab593dd5afa6d741d495
TheSupremeTaco <maksymlike@gmail.com>
2022-08-16 13:11:06 -0400

    Test

---
+++ src/main/java/javalessons/HW1/App.java
@@ -0,0 +1,13 @@
+package javalessons.HW1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
